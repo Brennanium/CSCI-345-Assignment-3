d2bc594ae7fdc1b8c0a92e577172bcda4d2977aa
Brennan Drew <nannerb156@gmail.com>
2020-11-19 19:48:26 -0800

    adding initil files and messing around with JavaFX stuff

---
+++ deadwood/src/App.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
+
+/* 
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = 
+            FXMLLoader.load(getClass().getResource("Painter.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+}
+ */
