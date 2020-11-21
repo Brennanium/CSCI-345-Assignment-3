cb253f67da81d708b97dd4dcde9c183c1253ad53
Brennan Drew <nannerb156@gmail.com>
2020-11-20 20:27:13 -0800

    moving resources and testing Contoller stuff

--- deadwood/src/Controller.java
+++ deadwood/src/Controller.java
@@ -1,15 +1,41 @@
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
 
 public class Controller {
 
     @FXML
-    private Label label;
-
+    private Label dayLabel;
+    @FXML
+    private ImageView boardImageView;
+    @FXML
+    private ImageView currentPlayerDiceImage;
+    @FXML
+    private Label currentPlayerNameLabel;
+    @FXML
+    private Label currentPlayerMoneyLabel;
+    @FXML
+    private Label currentPlayerCreditsLabel;
+    @FXML
+    private Label currentPlayerSuccessfullScenesLabel;
+    @FXML
+    private Label currentPlayerRankLabel;
+    @FXML
+    private Label currentPlayerCurrentAreaLabel;
+    @FXML
+    private Label currentPlayerCurrentRoleLabel;
+    @FXML
+    private Label currentPlayerPracticeChipsLabel;
+    
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        int day = 1;
+        dayLabel.setText("Day " + day);
+        
+        Image boardImage = new Image(getClass().getResourceAsStream("resources/board.jpg"));
+        boardImageView.setImage(boardImage);
+
+        Image diceImage = new Image(getClass().getResourceAsStream("resources/r2.png"));
+        currentPlayerDiceImage.setImage(diceImage);
     }
 }
