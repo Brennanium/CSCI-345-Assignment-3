e06e1c184c16da9cacd4d25905c4f2c72731b33f
Thannaree Trairaweewat <thannareetrairaweewat@Thannarees-MacBook-Pro.local>
2020-11-24 16:57:44 -0800

    adding mainGameController

--- deadwood/src/mainGameController.java
+++ deadwood/src/mainGameController.java
@@ -1,4 +1,5 @@
 import javafx.fxml.*;
+import javafx.geometry.Insets;
 import javafx.scene.*;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -6,16 +7,36 @@ import javafx.stage.Stage;
 
 public class mainGameController {
 
-    public void initialize(){
+    @FXML
+    private HBox mainBG;
+
+    public void initialize() throws Exception {
         Stage stage;
-        Parent root;
+        Parent board;
+        Parent playerInfo;
         
+        //stage = (Stage)
+        //stage =  
+        board = FXMLLoader.load(getClass().getResource("boardView.fxml"));
+        playerInfo = FXMLLoader.load(getClass().getResource("currentPlayerInfoView.fxml"));
+
+        Scene scene = new Scene(board);
+        
+        //stage.setScene(scene);
+        //stage.show();
+        //HBox mainBG = new HBox();
+       // mainBG.setId("currentPlayerInfoView.fxml");
+
+
+        
+    }
+
+    private Parent makeNewHBox(){
         HBox mainBG = new HBox();
-        mainBG.setId("currentPlayerInfoView.fxml");
-        mainBG.
+        mainBG.setPadding(new Insets(10, 0, 0, 10));
 
+        return mainBG;
 
-        
     }
 
 
