bbdb05e7eeef4c955f4a8857a1e80383c8c7b1a8
Brennan Drew <nannerb156@gmail.com>
2020-11-27 02:37:54 -0800

    cleaned up playerSelectScreenController and added duplicated and empty checking

--- deadwood/src/playerSelectScreenController.java
+++ deadwood/src/playerSelectScreenController.java
@@ -1,6 +1,7 @@
 
 import java.util.*;
 
+import javafx.beans.value.*;
 import javafx.collections.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
@@ -12,6 +13,13 @@ import javafx.stage.*;
 import model.*;
 
 public class playerSelectScreenController {
+    private final String textFieldErrorStyleText = 
+        "-fx-text-box-border: red ;" +
+        "-fx-focus-color: red ;";
+    private final String colorPickerErrorStyleText = 
+        "-fx-border-color: red ;" +
+        "-fx-focus-color: red ;";
+
     @FXML
     private ComboBox<Integer> numOfPlayersComboBox;
 
@@ -23,7 +31,9 @@ public class playerSelectScreenController {
 
     private ObservableList<Integer> listOfNums = FXCollections.observableArrayList();
 
-    private ArrayList<Parent> playerAndNumberPickerList = new ArrayList<Parent>();
+    private ArrayList<HBox> playerAndNumberPickerList = new ArrayList<HBox>();
+    private ArrayList<ComboBox<String>> colorPickerList = new ArrayList<ComboBox<String>>();
+    private ArrayList<TextField> textFieldList = new ArrayList<TextField>();
 
     private ObservableList<String> colors = FXCollections.observableArrayList();
     private HashMap<String, String> colorToHex = new HashMap<String,String>();
@@ -42,25 +52,39 @@ public class playerSelectScreenController {
         int numOfChoices = playerAndNumberPickerList.size();
 
         if (selection != null && numOfChoices == 0) {
-            Parent PACPV;
+            //add enough playerAndColorPickerViews 
+            //selection is new
             for(int i = 1; i <= selection; i++){
-                PACPV = makeNewPlayerAndColorPickerView(i);
-                playerAndNumberPickerList.add(PACPV);
-                PACPVStack.getChildren().add(PACPV);
+                addNewPlayerAndColorPickerView(i);
             }
         }
         else if (selection != null && selection != numOfChoices) {
+            //add new playerAndColorPickerViews
+            //selection is greater than current number of views
             if(selection - numOfChoices > 0) {
-                Parent PACPV;
                 for(int i = numOfChoices + 1; i <= selection; i++){
-                    PACPV = makeNewPlayerAndColorPickerView(i);
-                    playerAndNumberPickerList.add(PACPV);
-                    PACPVStack.getChildren().add(PACPV);
+                    addNewPlayerAndColorPickerView(i);
                 }
             } else {
+                //remove excess views
+                //selection is less than current number of views
                 while(selection - playerAndNumberPickerList.size() < 0){
-                    PACPVStack.getChildren().remove(
-                        playerAndNumberPickerList.remove(playerAndNumberPickerList.size()-1));
+                    HBox PACPV = playerAndNumberPickerList.remove(playerAndNumberPickerList.size()-1);
+                    ComboBox<String> colorPicker = colorPickerList.remove(colorPickerList.size() -1 );
+                    textFieldList.remove(textFieldList.size() -1 );
+
+                    String selectedColor = colorPicker.getSelectionModel().getSelectedItem();
+                    
+                    //add back the removed colors to other color pickers 
+                    if(selectedColor != null) {
+                        colorPickerList.stream()
+                            .filter(comboBox -> !comboBox.equals(colorPicker))
+                            .forEach(comboBox -> {
+                                comboBox.getItems().add(selectedColor);
+                            });
+                    }
+
+                    PACPVStack.getChildren().remove(PACPV);
                 }
             }
         }
@@ -86,7 +110,7 @@ public class playerSelectScreenController {
         }
     }
 
-    private Parent makeNewPlayerAndColorPickerView(int i){
+    private void addNewPlayerAndColorPickerView(int i){
         HBox container = new HBox();
         container.setPadding(new Insets(20, 0, 5, 0));
         container.setSpacing(5);
@@ -94,14 +118,85 @@ public class playerSelectScreenController {
         Label player = new Label();
         player.setText("Player " + i);
 
+        //setup text field
         TextField textField = new TextField();
+        textField.setPromptText("Name");
+        textField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                ArrayList<TextField> duplicates = new ArrayList<TextField>();
 
+                textFieldList.stream().filter((TextField tf) -> !tf.equals(textField)).forEach(tf -> {
+                    String otherText = tf.getText();
+                    if(otherText != null && !otherText.isEmpty()){ 
+                        if(otherText.equals(newValue)) {
+                            tf.setStyle(textFieldErrorStyleText);
+                            duplicates.add(tf);
+                        } else {
+                            int otherDuplicates = (int)textFieldList.stream().filter(t -> t.getText().equals(otherText)).count();
+                            if(otherText.equals(oldValue) && otherDuplicates <= 1){
+                                tf.getStyleClass().clear();
+                                tf.setStyle(null);
+                                tf.getStyleClass().addAll("text-field", "text-input");
+                            }
+                        }
+                    }
+                });
+                if(duplicates.size() != 0){
+                    textField.setStyle(textFieldErrorStyleText);
+                } else {
+                    textField.getStyleClass().clear();
+                    textField.setStyle(null);
+                    textField.getStyleClass().addAll("text-field", "text-input");
+                }
+            }
+        });
+        textFieldList.add(textField);
+
+
+        //setup color picker
         ComboBox<String> colorPicker = new ComboBox<String>();
-        colorPicker.setItems(colors);
+        colorPicker.getItems().addAll(getAvailableColors());
         colorPicker.setPromptText("Select color");
+        colorPicker.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                colorPickerList.stream().filter((ComboBox<String> comboBox) -> !comboBox.equals(colorPicker)).forEach(comboBox -> {
+                    comboBox.getItems().remove(newValue);
+        
+                    if (oldValue != null && !comboBox.getItems().contains(oldValue)){
+                        comboBox.getItems().add(oldValue);
+                    }
+                });
+
+                if(oldValue == null && newValue != null){
+                    colorPicker.getStyleClass().clear();
+                    colorPicker.setStyle(null);
+                    colorPicker.getStyleClass().addAll("combo-box", "combo-box-base");
+                }
+            }
+        });
+        colorPickerList.add(colorPicker);
 
         container.getChildren().addAll(player,textField,colorPicker);
-        return container;
+
+        playerAndNumberPickerList.add(container);
+        PACPVStack.getChildren().add(container);
+    }
+
+    private ArrayList<String> getAvailableColors(){
+        ArrayList<String> availableColors = new ArrayList<String>();
+        availableColors.addAll(colors);
+
+        String selection;
+        for(ComboBox<String> comboBox : colorPickerList) {
+            selection = comboBox.getSelectionModel().getSelectedItem();
+            if(selection != null) {
+                availableColors.remove(selection);
+            }
+        }
+
+        return availableColors;
     }
 
     private void setupColors(){
@@ -119,16 +214,50 @@ public class playerSelectScreenController {
     }
 
     private boolean ready(){
-        return !playerAndNumberPickerList.isEmpty() &&
-            !playerAndNumberPickerList.stream()
-                .anyMatch((Parent p) -> {
-                    for(Node n : p.getChildrenUnmodifiable()) {
-                        if(n instanceof TextField) {
-                            return ((TextField)n).getText().isEmpty();
-                        }
-                    }
-                    return true;
-                });
+        if(playerAndNumberPickerList.isEmpty())
+            return false;
+
+        boolean emptyText = existsEmptyTextField();
+        boolean emptyColor = existsEmptyColorPicker();
+
+            
+        return !emptyText && !emptyColor && !hasDuplicateName();
+            
+    }
+
+    private boolean existsEmptyTextField() {
+        boolean hasEmpty = false;
+        for(TextField tf : textFieldList) {
+            if(tf.getText().isEmpty()) {
+                hasEmpty = true;
+                tf.setStyle(textFieldErrorStyleText);
+            }
+        }
+        return hasEmpty;
+    }
+
+    private boolean hasDuplicateName() {
+        boolean hasDuplicate;
+        
+        for(TextField textField : textFieldList) {
+            hasDuplicate = textFieldList.stream()
+                .filter(tf -> !tf.equals(textField))
+                .anyMatch(tf -> tf.getText().equals(textField.getText()));
+            if(hasDuplicate)
+                return true;
+        }
+        return false;
+    }
+
+    private boolean existsEmptyColorPicker() {
+        boolean hasEmpty = false;
+        for(ComboBox<String> cb : colorPickerList) {
+            if(cb.getSelectionModel().getSelectedItem() == null) {
+                hasEmpty = true;
+                cb.setStyle(colorPickerErrorStyleText);
+            }
+        }
+        return hasEmpty;
     }
 
     private ArrayList<Player> getPlayers(){
